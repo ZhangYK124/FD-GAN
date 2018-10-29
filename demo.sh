d534814091ac357d7ba15ed7fb22889d3450a046
zzd <zdzheng12@gmail.com>
2018-10-29 10:04:43 -0700

    forward

---
+++ demo.sh
@@ -0,0 +1,6 @@
+python test.py --display-port 6006 --display-id 1 \
+    --stage 2 -d market1501 \
+    --pose-aug gauss -b 32 -j 4 --niter 25 --niter-decay 25 --lr 0.0001 --save-step 10 --eval-step 5 \
+    --lambda-recon 100.0 --lambda-veri 10.0 --lambda-sp 10.0 --smooth-label \
+    --netE-pretrain ./model/stageIII/best_net_E.pth --netG-pretrain ./model/stageIII/best_net_G.pth \
+    --netDi-pretrain ./model/stageIII/best_net_Di.pth --netDp-pretrain ./model/stageIII/best_net_Dp.pth
